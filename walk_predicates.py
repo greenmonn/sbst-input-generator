3291171e43f711c4303d364dc5562d449bf8eecd
greenmon <greenmon@kaist.ac.kr>
2019-05-05 02:27:03 +0900

    Change predicates tree to have all branches
    Change predicates tree to have all true/false branches.

--- walk_predicates.py
+++ walk_predicates.py
@@ -1,3 +1,4 @@
+import ast
 import astor
 from anytree import AnyNode
 
@@ -6,15 +7,69 @@ class WalkPredicates(astor.TreeWalk):
         astor.TreeWalk.__init__(self)
 
         self.predicates_stack = []
-        self.predicates_tree_root = AnyNode(ast_node=None)
+        self.predicates_tree_root = AnyNode(ast_node=None, id=None, type=None)
+        self.cur_branch_id = 0
+
+        self.false_branches_stack = []
+
+    def add_trace_argument(self):
+        pass
+    
+
+    def pre_FunctionDef(self):
+        trace = ast.copy_location(ast.arg(arg='trace', annotation=None), self.cur_node)
+        self.cur_node.args.args.append(trace)
+
+    def _inject_trace_hook(self, compare_node, branch_id):
+        op = ''
+        lhs = astor.to_source(compare_node.left).rstrip()
+        rhs = astor.to_source(compare_node.comparators[0]).rstrip()
+
+        if isinstance(compare_node.ops[0], ast.Gt):
+            op = 'greater_than'
+        elif isinstance(compare_node.ops[0], ast.GtE):
+            op = 'greater_than_or_equals'
+        elif isinstance(compare_node.ops[0], ast.Lt):
+            op = 'less_than'
+        elif isinstance(compare_node.ops[0], ast.LtE):
+            op = 'less_than_or_equals'
+        elif isinstance(compare_node.ops[0], ast.Eq):
+            op = 'equals'
+        elif isinstance(compare_node.ops[0], ast.NotEq):
+            op = 'not_equals'
+
+        f_call = 'trace.{fname}({branch_id}, {lhs}, {rhs})'.format(fname=op, branch_id=branch_id, lhs=lhs, rhs=rhs)
+
+        f_call_node = ast.parse(f_call, '', 'eval').body
+
+        return f_call_node
 
     def pre_If(self):
+        self.cur_branch_id += 1
+        
         parent = self.predicates_tree_root
         if len(self.predicates_stack) > 0:
-            parent = self.predicates_stack[-1]
+            true_node, false_node = self.predicates_stack[-1]
+            if len(self.false_branches_stack) == 0:
+                parent = true_node
 
-        node = AnyNode(ast_node=self.cur_node, parent=parent)
-        self.predicates_stack.append(node)
+            elif self.false_branches_stack[-1] == self.cur_branch_id - 1:
+                parent = false_node
+            else:
+                parent = true_node
+
+        self.cur_node.test = self._inject_trace_hook(self.cur_node.test, self.cur_branch_id)
+
+        true_branch_node = AnyNode(id=self.cur_branch_id, type=True, ast_node=self.cur_node, parent=parent)
+        false_branch_node = AnyNode(id=self.cur_branch_id, type=False, ast_node=self.cur_node, parent=parent)
+
+        self.predicates_stack.append((true_branch_node, false_branch_node))
+
+    def pre_orelse_name(self):
+        self.false_branches_stack.append(self.cur_branch_id)
+    
+    def post_orelse_name(self):
+        self.false_branches_stack.pop()
 
     def post_If(self):
         self.predicates_stack.pop()
